25c180670d378109f4089fc1a5e01ec19cc22d33
Pavel Simek <pavel.simek@shopsys.com>
2020-01-07 07:02:19 +0100

    set coding standards

--- RoboFile.php
+++ RoboFile.php
@@ -1,10 +1,20 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 class RoboFile extends \Robo\Tasks
 {
-    public function tests()
+    public function tests(): void
     {
+        $this
+            ->taskExec(__DIR__ . '/vendor/bin/ecs check --config ./config/ecs.yaml .')
+            ->run();
+    }
+
+    public function fixStandards(): void
+    {
+        $this
+            ->taskExec(__DIR__ . '/vendor/bin/ecs check --config ./config/ecs.yaml --fix .')
+            ->run();
     }
 }
