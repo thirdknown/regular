67e1e6eefb2f9b78f90a805ce52bb7ebaa9c2be3
Pavel Simek <pavel.simek@shopsys.com>
2020-01-11 21:11:54 +0100

    tests: add border instance of interfaces test

--- tests/Border/BorderTest.php
+++ tests/Border/BorderTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 use Thirdknown\Regular\Border\Delimiter;
+use Thirdknown\Regular\Border\DelimiterInterface;
 use Thirdknown\Regular\Border\EndOfLine;
 use Thirdknown\Regular\Border\PlusDelimiter;
 use Thirdknown\Regular\Border\SlashDelimiter;
@@ -18,5 +19,12 @@ class BorderTest extends TestCase
         $this->assertSame('+', (new PlusDelimiter())->__toString());
         $this->assertSame('/', (new SlashDelimiter())->__toString());
         $this->assertSame('^', (new StartOfLine())->__toString());
+    }
+
+    public function testInstanceOfInterfaces(): void
+    {
+        $this->assertInstanceOf(DelimiterInterface::class, new Delimiter('/'));
+        $this->assertInstanceOf(DelimiterInterface::class, new PlusDelimiter());
+        $this->assertInstanceOf(DelimiterInterface::class, new SlashDelimiter());
     }
 }
