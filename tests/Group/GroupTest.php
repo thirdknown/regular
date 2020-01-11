cf6f19f95a04113f77671c18ecefa3a8a1496bf9
Pavel Simek <pavel.simek@shopsys.com>
2020-01-11 22:11:55 +0100

    tests: add group tests

---
+++ tests/Group/GroupTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use Thirdknown\Regular\Abbreviation\NumberCharacter;
+use Thirdknown\Regular\Expression\Expression;
+use Thirdknown\Regular\Group\CloseRoundBracket;
+use Thirdknown\Regular\Group\Group;
+use Thirdknown\Regular\Group\OpenRoundBracket;
+use Thirdknown\Regular\Group\VerticalBar;
+
+class GroupTest extends TestCase
+{
+    public function testGroup(): void
+    {
+        $group = new Group();
+        $group
+            ->addExpression(new NumberCharacter())
+            ->addExpression(new Expression('c'))
+            ->addExpression(new Expression('#'));
+        $this->assertSame('(\d|c|#)', $group->__toString());
+    }
+
+    public function testSimpleExpression(): void
+    {
+        $this->assertSame('(', (new OpenRoundBracket())->__toString());
+        $this->assertSame(')', (new CloseRoundBracket())->__toString());
+        $this->assertSame('|', (new VerticalBar())->__toString());
+    }
+}
