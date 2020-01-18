8b4bda25047a8a91fc209235f49ae61b5169a6e2
Pavel Simek <pavel.simek@shopsys.com>
2020-01-18 11:00:14 +0100

    abbreviations: add Space

---
+++ src/Thirdknown/Regular/Abbreviation/Space.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Thirdknown\Regular\Abbreviation;
+
+use Thirdknown\Regular\Quantifier\QuantifiableInterface;
+use Thirdknown\Regular\Quantifier\QuantifierTrait;
+
+class Space implements AbbreviationInterface, QuantifiableInterface
+{
+    use QuantifierTrait;
+
+    public const EXPRESSION = ' ';
+
+    public function __toString(): string
+    {
+        return self::EXPRESSION . ($this->getQuantifier() ?? '');
+    }
+
+    public static function create(): self
+    {
+        return new self();
+    }
+}
--- tests/Abbreviation/AbbreviationTest.php
+++ tests/Abbreviation/AbbreviationTest.php
@@ -6,6 +6,7 @@ use PHPUnit\Framework\TestCase;
 use Thirdknown\Regular\Abbreviation\Dot;
 use Thirdknown\Regular\Abbreviation\NonWhitespaceCharacter;
 use Thirdknown\Regular\Abbreviation\NumberCharacter;
+use Thirdknown\Regular\Abbreviation\Space;
 
 class AbbreviationTest extends TestCase
 {
@@ -14,5 +15,6 @@ class AbbreviationTest extends TestCase
         $this->assertSame('.', (new Dot())->__toString());
         $this->assertSame('\S', (new NonWhitespaceCharacter())->__toString());
         $this->assertSame('\d', (new NumberCharacter())->__toString());
+        $this->assertSame(' ', (new Space())->__toString());
     }
 }
